00d01f07e2bd022119afbb6cfca22db92f3d14d0
Guy Marino <gxm262@case.edu>
2020-09-21 14:14:40 -0400

    Copy old code files

---
+++ Chapter 1/1.3 -  Load More Code/boot.asm
@@ -0,0 +1,33 @@
+;
+; Hello, BIOS
+;
+; boot.asm
+;
+
+; Set Program Origin
+[org 0x7C00]
+
+; 16-bit Mode
+[bits 16]
+
+; Print Message
+mov bx, msg_hello_world
+call print_bios
+
+; Infinite Loop
+bootsector_hold:
+jmp $               ; Infinite loop
+
+; INCLUDES
+%include "print.asm"
+
+; DATA STORAGE AREA
+
+; String Message
+msg_hello_world:                db `\r\nHello World, from the BIOS!\r\n`, 0
+
+; Pad boot sector for magic number
+times 510 - ($ - $$) db 0x00
+
+; Magic number
+dw 0xAA55
