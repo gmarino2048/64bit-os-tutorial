ce9b5f4061752b9bf2f0deaffa0f67778aa7c7bb
Guy Marino <gxm262@case.edu>
2020-11-11 14:51:32 -0500

    Add routine for print function

--- Chapter 1/1.5 - Protected Mode/protected_mode/print.asm
+++ Chapter 1/1.5 - Protected Mode/protected_mode/print.asm
@@ -5,3 +5,37 @@
 ;
 
 [bits 32]
+
+; Simple 32-bit protected print routine
+; Message address stored in esi
+print_protected:
+    pusha
+    mov edx, vga_start
+
+    ; Do main loop
+    print_protected_loop:
+        ; If char == \0, string is done
+        cmp byte[esi], 0
+        je  print_protected_done
+
+        ; Move character to al, style to ah
+        mov al, byte[esi]
+        mov ah, style_wb
+
+        ; Print character to vga memory location
+        mov word[edx], ax
+
+        ; Increment counter registers
+        add esi, 1
+        add edx, 2
+
+        ; Redo loop
+        jmp print_protected_loop
+
+print_protected_done:
+    popa
+    ret
+
+; Define necessary constants
+vga_start:                  equ 0x000B8000
+style_wb:                   equ 0x0F
