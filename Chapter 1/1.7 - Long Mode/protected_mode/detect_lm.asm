39885b7e5b249016442ce16d9ea1b05ec1992168
Guy Marino <gxm262@case.edu>
2020-11-17 16:50:23 -0500

    Remove large comments

--- Chapter 1/1.7 - Long Mode/protected_mode/detect_lm.asm
+++ Chapter 1/1.7 - Long Mode/protected_mode/detect_lm.asm
@@ -6,42 +6,11 @@
 
 [bits 32]
 
-; Detecting Long Mode
-;
-; Long mode is supported on all 64-bit CPUs, but not
-; on 32-bit CPUs. However, since we've been in protected
-; mode, we have no idea which type of system we're running
-; on. We need to figure out if we can even use long mode so
-; that we don't cause a ton of errors when trying to activate
-; it.
+; Detect Long Mode
 detect_lm_protected:
     pushad
 
-    ; In order to detect long mode, we need to use the built-in command
-    ; "cpuid". However, CPUID has different modes, and some CPUS don't
-    ; support the additional functionalities to check for long mode, even
-    ; if they support CPUID. So this makes the check for long mode way more
-    ; convoluted than it needs to be. Once again, the steps are:
-    ;
-    ; 1. Check for CPUID
-    ; 2. Check for CPUID extended functions
-    ; 3. Check for long mode support
-    
-
-    ; Checking for CPUID
-    ;
-    ; We can check for the existence of CPUID by using a bit in the
-    ; FLAGS register. This register cannot be accessed by the mov command,
-    ; so we must actually push it onto the stack and pop it off to read or
-    ; write to it.
-    ;
-    ; The bit that tells us if CPUID is supported is bit 21. IF CPUID is
-    ; NOT supported, then this bit MUST take on a certain value. However,
-    ; if CPUID is supported, then it'll take on whatever value we give it.
-    ; We can try this by reading from FLAGS, flipping the bit, writing to
-    ; FLAGS, and then reading again and comparing to the earlier read. If
-    ; they're equal, then the bit is 0
-
+    ; Check for CPUID
     ; Read from FLAGS
     pushfd                          ; Copy FLAGS to eax via stack
     pop eax
@@ -73,14 +42,6 @@ detect_lm_protected:
 
 
     ; Check for extended functions of CPUID
-    ;
-    ; Now that we know CPUID exists, we can use it to
-    ; see whether it supports the extended functions needed
-    ; to enable long mode. The CPUID function takes an argument
-    ; in eax and returns the value in eax. To check for extended
-    ; capabilities, we use the argument mov eax, 0x80000000. If
-    ; extended capabilities exist, then the value will be set to
-    ; greater than 0x80000000, otherwise it will stay the same.
     mov eax, 0x80000000             ; CPUID argument than 0x80000000
     cpuid                           ; Run the command
     cmp eax, 0x80000001             ; See if result is larger than than 0x80000001
@@ -88,10 +49,6 @@ detect_lm_protected:
 
 
     ; Actually check for long mode
-    ;
-    ; Now, we can use CPUID to check for long mode. If long mode is
-    ; supported, then CPUID will set the 29th bit of register edx. The
-    ; eax code to look for long mode is 0x80000001
     mov eax, 0x80000001             ; Set CPUID argument
     cpuid                           ; Run CPUID
     test edx, 1 << 29               ; See if bit 29 set in edx
@@ -101,18 +58,16 @@ detect_lm_protected:
     popad
     ret
 
-;
+
 ; Print an error message and hang
-;
 cpuid_not_found_protected:
     call clear_protected
     mov esi, cpuid_not_found_str
     call print_protected
     jmp $
 
-;
+
 ; Print an error message and hang
-;
 lm_not_found_protected:
     call clear_protected
     mov esi, lm_not_found_str
--- Chapter 1/1.7 - Long Mode/protected_mode/init_pt.asm
+++ Chapter 1/1.7 - Long Mode/protected_mode/init_pt.asm
@@ -4,86 +4,23 @@
 ; init_pt.asm
 ;
 
-; Initialize the page table
-; 
-; The Page Table has 4 components which will be mapped as follows:
-;
-; PML4T -> 0x1000 (Page Map Level 4 Table)
-; PDPT  -> 0x2000 (Page Directory Pointer Table)
-; PDT   -> 0x3000 (Page Directory Table)
-; PT    -> 0x4000 (Page table)
-;
-; Clear the memory in those areas and then set up the page table structure
-
-; Each table in the page table has 512 entries, all of which are 8 bytes
-; (one quadword or 64 bits) long. In this step we'll be identity mapping
-; ONLY the lowest 2 MB of memory, since this is all we need for now. Note
-; that this only requires one page table, so the upper 511 entries in the
-; PML4T, PDPT, and PDT will all be NULL.
-
-; Once we have the zeroth address in all pointing to our page table, we
-; will need to create a identity map, which will point each virtual page
-; to the physical page accessed with that address. Note that in the x86_64
-; architecture, a page is addressed using 12 bits, which corresponds to
-; 4096 addressible bytes (4KB). Remember this, it'll be important later.
-
-; Another thing to note is that we're in protected mode now, which grants
-; us access to fancy CISC instructions like rep, stosd, and loop. I'll
-; explain what each of these do a bit later in the file.
-
 [bits 32]
 
+; Initialize the page table
 init_pt_protected:
     pushad
 
-    ; NOTE: We did not set up 32-bit paging because we wanted to jump directly
-    ; into long mode. You should know that this is possible, and if you've done
-    ; it before entering long mode you will need to disable it here. To do so,
-    ; clear the 31st bit of the cr0 register.
-
     ; Clear the memory area using rep stosd
-    ;
-    ; rep stosd is what's known as a "repeating string command", meaning that it
-    ; will write the same thing over and over until a certain criteria is met.
-    ; How does it know when to stop though? We tell it with the eax, edi, and ecx
-    ; registers. eax is the value to write, edi is the start address, and ecx is the
-    ; number of repetitions to perform.
-    ;
-    ; Even the instruction has significance. The 'd' at the end of 'stosd' tells the
-    ; cpu to write a "double word" or 4 bytes with each repetition (the same size as eax).
-    ; It also increments edi by 4 rather than by 1 to ensure no data overlap. So let's
-    ; get into the function then:
-    mov edi, 0x1000         ; Set the base address for rep stosd. Our page table goes from
-                            ; 0x1000 to 0x4FFF, so we want to start at 0x1000
-
-    mov cr3, edi            ; Save the PML4T start address in cr3. This will save us time later
-                            ; because cr3 is what the CPU uses to locate the page table entries
-
-    xor eax, eax            ; Set eax to 0. Note that xor is actually faster than "mov eax, 0".
-
-    mov ecx, 4096           ; Repeat 4096 times. Since each page table is 4096 bytes, and we're
-                            ; writing 4 bytes each repetition, this will zero out all 4 page tables
-
-    rep stosd               ; Now actually zero out the page table entries
+    mov edi, 0x1000
+    mov cr3, edi
+    xor eax, eax
+    mov ecx, 4096
+    rep stosd
 
     ; Set edi back to PML4T[0]
     mov edi, cr3
 
     ; Set up the first entry of each table
-    ;
-    ; This part can be a little confusing, and it took me a while to understand.
-    ; The key is knowing that the page tables MUST be page aligned. This means
-    ; the lower 12 bits of the physical address (3 hex digits) MUST be 0. Then,
-    ; each page table entry can use the lower 12 bits as flags for that entry.
-    ;
-    ; You may notice that we're setting our flags to "0x003", because we care most
-    ; about bits 0 and 1. Bit 0 is the "exists" bit, and is only set if the entry
-    ; corresponds to another page table (for the PML4T, PDPT, and PDT) or a page of
-    ; physical memory (in the PT). Obviously we want to set this. Bit 1 is the
-    ; "read/write" bit, which allows us to view and modify the given entry. Since we
-    ; want our OS to have full control, we'll set this as well.
-    ;
-    ; Now let's wire up our table. Note that edi is already at PML4T[0]
     mov dword[edi], 0x2003      ; Set PML4T[0] to address 0x2000 (PDPT) with flags 0x0003
     add edi, 0x1000             ; Go to PDPT[0]
     mov dword[edi], 0x3003      ; Set PDPT[0] to address 0x3000 (PDT) with flags 0x0003
@@ -92,30 +29,15 @@ init_pt_protected:
 
     ; Fill in the final page table
     ; NOTE: edi is at 0x3000
-    ;
-    ; We now want to make an Identity Mapping in our PT. We still want to have the flags
-    ; set to 0x0003 as shown above, but we want to set PT[0].addr to 0x00, PT[1].addr to
-    ; 0x01, etc. We'll do this using the "loop" command. In 16 bit mode, we had to program
-    ; loops ourselves using comparison operators, but now we can just use a single command.
-    ;
-    ; The "loop" command is essentially equivalent to the following pseudocode:
-    ; while (ecx > 0){
-    ;   {instructions}
-    ;   ecx --
-    ; }
-    ; Or, more simply: "Do {instructions} ecx times, and decrement ecx each time".
-    ; We can use this loop command to fill in all 512 entries of the page table as follows:
     add edi, 0x1000             ; Go to PT[0]
     mov ebx, 0x00000003         ; EBX has address 0x0000 with flags 0x0003
     mov ecx, 512                ; Do the operation 512 times
 
     add_page_entry_protected:
-        ; a = address, x = index of page table, flags are entry flags
-        mov dword[edi], ebx                 ; Write ebx to PT[x] = a.append(flags)
-        add ebx, 0x1000                     ; Increment address of ebx (a+1)
-        add edi, 8                          ; Increment page table location (since entries are 8 bytes)
-                                            ; x++
-        loop add_page_entry_protected       ; Decrement ecx and loop again
+        mov dword[edi], ebx
+        add ebx, 0x1000
+        add edi, 8
+        loop add_page_entry_protected
 
     ; Now we should have a page table that identities maps the lowest 2MB of physical memory into
     ; virtual memory!
