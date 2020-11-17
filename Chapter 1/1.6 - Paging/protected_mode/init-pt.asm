b4833ff7d3a1b739a0570101525bb88bd2f686e2
Guy Marino <gxm262@case.edu>
2020-11-16 23:05:57 -0500

    Add code to fully initialize the page table

--- Chapter 1/1.6 - Paging/protected_mode/init-pt.asm
+++ Chapter 1/1.6 - Paging/protected_mode/init-pt.asm
@@ -4,3 +4,48 @@
 ; init-pt.asm
 ;
 
+;
+; Initialize the page table
+; 
+; The Page Table has 4 components which will be mapped as follows:
+;
+; PML4T -> 0x1000
+; PDPT  -> 0x2000
+; PDT   -> 0x3000
+; PT    -> 0x4000
+;
+; Clear the memory in those areas and then set up the page structure
+; 
+
+init_pt_protected:
+    pusha
+
+    ; Clear the memory area
+    mov edi, 0x1000
+    mov cr3, edi
+    xor eax, eax
+    mov ecx 4096
+    rep stosd
+    mov edi, cr3
+
+    ; Set up the first entry of each page
+    mov dword[edi], 0x2003
+    add edi, 0x1000
+    mov dword[edi], 0x3003
+    add edi, 0x1000
+    mov dword[edi], 0x4003
+
+    ; Fill in the final page
+    ; NOTE: edi is at 0x3000
+    add edi, 0x1000
+    mov ebx, 0x00000003
+    mov ecx, 512
+
+    add_page_entry_protected:
+        mov dword[edi], ebx
+        add ebx, 0x1000
+        add edi, 8
+        loop add_page_entry_protected
+
+    popa
+    ret
