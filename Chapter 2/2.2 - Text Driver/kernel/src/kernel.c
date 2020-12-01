60ff1c2501228a087e623aa6db7bf75f215d4b45
Guy Marino <gxm262@case.edu>
2020-12-01 00:09:15 -0500

    Update message

--- Chapter 2/2.2 - Text Driver/kernel/src/kernel.c
+++ Chapter 2/2.2 - Text Driver/kernel/src/kernel.c
@@ -3,11 +3,19 @@
 
 int main() {
     set_cursor_pos(0, 0);
-    clearwin(COLOR_BLK, COLOR_CYN);
+    clearwin(COLOR_BLK, COLOR_LCY);
 
-    const char *welcome = "Now we have a more advanced vga driver that does what we want!";
+    const char *first = "Now we have a more advanced vga driver that does what we want! ";
 
-    putstr(welcome, COLOR_BLK, COLOR_CYN);
+    putstr(first, COLOR_BLK, COLOR_LCY);
+
+    const char *second = "It even wraps the text around the screen and moves the cursor correctly. ";
+
+    putstr(second, COLOR_BLK, COLOR_LCY);
+
+    const char *third = "But if we reach the end of the screen it still doesn't quite scroll properly...";
+
+    putstr(third, COLOR_BLK, COLOR_LCY);
     
     return 0;
 }
